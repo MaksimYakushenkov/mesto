afceaa14f65fa6a617512fdeb3c81732de9e7cd8
Maksim Yakushenkov <breakingwebnews@icloud.com>
2022-05-07 17:39:20 +0300

    feat: Практическая работа №9

--- src/components/PopupDelete.js
+++ src/components/PopupDelete.js
@@ -4,7 +4,7 @@ export default class PopupDelete extends Popup {
   constructor (popupSelector, handleCardDelete) {
     super(popupSelector);
     this._handleCardDelete = handleCardDelete;
-    this._deleteConfirm = this._popup.querySelector('.delete-popup');
+    this._deleteConfirm = this._popup.querySelector('.popup__confirm-delete');
   }
 
   setEventListeners(id) {
--- src/pages/index.js
+++ src/pages/index.js
@@ -147,7 +147,7 @@ editProfileButton.addEventListener('click', function () {
 const imagePopup = new PopupWithImage('.image-popup');
 
 // Создание класса попапа удаления карточки
-const deletePopup = new PopupDelete('.popup__delete_card', (id) => {
+const deletePopup = new PopupDelete('.delete-popup', (id) => {
   api.deleteUserCard(id, {
       authorization: 'f4ccf291-bfa6-4f7d-8768-52c052475176'
   })
